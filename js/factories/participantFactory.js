ee5e8f9311dbde98bf2b844da049103806e26634
navi9000 <icdi7434@gmail.com>
2026-05-10 16:44:42 +0400

    participants, footer - fullscreen

--- js/factories/participantFactory.js
+++ js/factories/participantFactory.js
@@ -1,3 +1,4 @@
+import { participantList } from "../data.js"
 import Factory from "../factory.js"
 import Templates from "../templates.js"
 import { qs } from "../utils/dom.js"
@@ -5,17 +6,24 @@ import { populate } from "../utils/templates.js"
 
 class ParticipantFactory extends Factory {
   #$container = qs(".participants__list-container")
+  #participantList = participantList.map(({ name, description, src }) =>
+    populate(Templates.ParticipantItem(), {
+      name,
+      description,
+      src: src ?? "img/participant-placeholder.png",
+    }),
+  )
 
   renderDesktop() {
-    this.#$container.innerHTML = "Большой экран"
+    this.#$container.innerHTML = "".concat(
+      this.#participantList[0],
+      this.#participantList[1],
+      this.#participantList[2],
+    )
   }
 
   renderMobile() {
-    this.#$container.innerHTML = populate(Templates.ParticipantItem(), {
-      src: "img/participant-placeholder.png",
-      name: "Хозе-Рауль Капабланка",
-      description: "Чемпион мира по шахматам",
-    })
+    this.#$container.innerHTML = this.#participantList[0]
   }
 }
 
